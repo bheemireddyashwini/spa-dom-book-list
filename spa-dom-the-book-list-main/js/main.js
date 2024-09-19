c39357c3bbcb3c36ec5e5315c2a7d880fc071f1f
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2024-09-19 09:46:04 +0200

    first commit

---
+++ spa-dom-the-book-list-main/js/main.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    title: "The Design of EveryDay Things",
+    author: "Norman, Don",
+    img: "images/camp-7103189_640.png",
+  },
+  {
+    title: "The Most Human Human",
+    author: "Christian, Brian",
+    img: "images/cupcakes-2887270_640.jpg",
+  },
+  {
+    title: "Thinking with Type",
+    author: "Lupton, Ellen",
+    img: "images/manufacturing-35087_640.png",
+  },
+  {
+    title: "Eloquent JavaScript",
+    author: "Haverbeke, Marijn",
+    img: "images/snake-eagle-6591348_640.jpg",
+  },
+];
